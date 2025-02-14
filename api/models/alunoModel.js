09e16a4d5e095eb4ff3cfcbac718b042ab227f3b
Zee <zee@debian.ifrn.local>
2025-02-14 13:59:24 -0300

    Models

--- api/models/alunoModel.js
+++ api/models/alunoModel.js
@@ -1,1 +1,16 @@
-'use strict';
+const { DataType, DataTypes } = require('sequelize');
+const sequelize = require('../../database');
+
+const Aluno = sequelize.define('Aluno', {
+    idaluno: {
+        type : DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Aluno;
