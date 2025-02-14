419d5d9ad607171dc8f5b9fac6f0b53b1d331c8e
Zee <zee@debian.ifrn.local>
2025-02-14 14:07:41 -0300

    sequelize para tratar erros

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000;
+const sequelize = require('./database');
+const Aluno = require('./api/models/alunoModel')
+
+sequelize.sync()
+    .then(() => {
+        console.log('BD acessado');
+    })
+    .catch(err => {
+        console.log('Falha ao conectar com o DB', err);
+    });
+
 app.listen(port);
 
 app.get('/', function (req, res) { res.json({ hello: 'Olá Pessoa Estranha' }); })
